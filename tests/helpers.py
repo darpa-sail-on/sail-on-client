35d8a4ecfbeb6ca93ea9614092763f78daec0163
as6520 <ameya.shringi@kitware.com>
2020-09-02 09:23:39 -0400

    Add a fixture for discovering plugins

--- tests/helpers.py
+++ tests/helpers.py
@@ -1,12 +1,15 @@
-import multiprocessing
 import pytest
 import os
 import time
 import shutil
 import json
 import ubelt as ub
+import multiprocessing
+import logging
 from pathlib import Path
 from tempfile import TemporaryDirectory
+from pkg_resources import iter_entry_points
+from pkg_resources import DistributionNotFound
 
 from sail_on.api import server
 from sail_on.api.file_provider import FileProvider
@@ -14,8 +17,7 @@ from sail_on.api.file_provider import FileProvider
 @pytest.fixture(scope="function")
 def server_setup():
     """
-    Fixture to setup server and remove artifacts associated with the server after
-    the test
+    Fixture to setup server and remove artifacts associated with the server after the test
     """
     data_dir = Path(f"{os.path.dirname(__file__)}/data")
     result_dir = Path(f"{os.path.dirname(__file__)}/server_results_{time.time()}")
@@ -44,3 +46,19 @@ def get_interface_params():
         json.dump(dummy_config,
                   open(os.path.join(config_folder, config_name), 'w'))
         yield config_folder, config_name
+
+@pytest.fixture(scope="function")
+def discoverable_plugins():
+    """
+    Fixture to replicate plugin discovery from framework
+
+    TODO: Replace this with a function call from framework
+    """
+    discovered_plugins = {}
+    for entry_point in iter_entry_points('framework'):
+        try:
+            ep = entry_point.load()
+            discovered_plugins[entry_point.name] = ep
+        except (DistributionNotFound, ImportError):
+            logging.exception(f"Plugin {entry_point.name} not found")
+    return discovered_plugins
