892410932ac7a348fcb2d7c36b21c06432930347
as6520 <ameya.shringi@kitware.com>
2020-09-07 12:41:01 -0400

    Fix formating changes

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,20 +12,21 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('.'))
-sys.path.insert(0, os.path.abspath('..'))
+
+sys.path.insert(0, os.path.abspath("."))
+sys.path.insert(0, os.path.abspath(".."))
 
 
 # -- Project information -----------------------------------------------------
 
-project = 'Sail-On Client'
-copyright = '2020, Kitware INC.'
-author = 'Kitware INC.'
+project = "Sail-On Client"
+copyright = "2020, Kitware INC."
+author = "Kitware INC."
 
 # TODO: Get the version from the package
 # The full version, including alpha/beta/rc tags
-release = '0.1'
-version = '0.1'
+release = "0.1"
+version = "0.1"
 
 # -- General configuration ---------------------------------------------------
 
@@ -33,15 +34,15 @@ version = '0.1'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-        "sphinx.ext.autodoc",
-        "sphinx.ext.intersphinx",
-        "sphinx.ext.napoleon",
-        "sphinx_autodoc_typehints",
-        "sphinx.ext.autosectionlabel"
+    "sphinx.ext.autodoc",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.napoleon",
+    "sphinx_autodoc_typehints",
+    "sphinx.ext.autosectionlabel",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -63,9 +64,9 @@ pygments_style = "sphinx"
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'press'
+html_theme = "press"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
