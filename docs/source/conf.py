454334755af3a83068aef9f543ce4b5abc902a98
as6520 <ameya.shringi@kitware.com>
2020-09-07 10:12:46 -0400

    Add auto section label to conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -36,7 +36,8 @@ extensions = [
         "sphinx.ext.autodoc",
         "sphinx.ext.intersphinx",
         "sphinxcontrib.napoleon",
-        "sphinx_autodoc_typehints"
+        "sphinx_autodoc_typehints",
+        "sphinx.ext.autosectionlabel"
 ]
 
 # Add any paths that contain templates here, relative to this directory.
