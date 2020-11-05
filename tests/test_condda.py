33e2882ab03b38e5f963e12d9beb8d1d020d84c3
ameya.shringi <ameya.shringi@kitware.com>
2020-11-05 09:33:43 -0500

    Add tests for feature extraction

--- tests/test_condda.py
+++ tests/test_condda.py
@@ -23,6 +23,22 @@ def condda_config():
         json.dump(dummy_config, open(os.path.join(config_folder, config_name), "w"))
         yield os.path.join(config_folder, config_name)
 
+@pytest.fixture(scope="function")
+def condda_config_with_feature_extraction():
+    """Fixture to create a config file for feature extraction"""
+    with TemporaryDirectory() as feature_dir:
+        with TemporaryDirectory() as config_folder:
+            dummy_config = {
+                "domain": "image_classification",
+                "test_ids": ["CONDDA.1.1.1234"],
+                "novelty_detector_class": "MockDetector",
+                "feature_extraction_only": True,
+                "save_features": True,
+                "feature_save_dir": feature_dir,
+            }
+            config_name = "test_condda_config.json"
+            json.dump(dummy_config, open(os.path.join(config_folder, config_name), "w"))
+            yield os.path.join(config_folder, config_name)
 
 def test_initialize(
     server_setup, get_interface_params, discoverable_plugins, condda_config
@@ -67,4 +83,27 @@ def test_run_protocol(
     condda.run_protocol()
     local_interface = LocalInterface(config_name, config_directory)
     condda = Condda(discoverable_plugins, "", local_interface, condda_config)
+    condda.run_protocol()
+
+def test_feature_extraction(
+    server_setup, get_interface_params, discoverable_plugins, condda_config_with_feature_extraction
+):
+    """
+    Test feature extraction.
+
+    Args:
+        server_setup (tuple): Tuple containing url and result directory
+        get_interface_params (tuple): Tuple to configure par interface
+        discoverable_plugins (dict): Dictionary with the plugins
+        condda_config_with_feature_extraction (str): Path to json file
+
+    Return:
+        None
+    """
+    config_directory, config_name = get_interface_params
+    par_interface = ParInterface(config_name, config_directory)
+    condda = Condda(discoverable_plugins, "", par_interface, condda_config_with_feature_extraction)
+    condda.run_protocol()
+    local_interface = LocalInterface(config_name, config_directory)
+    condda = Condda(discoverable_plugins, "", local_interface, condda_config_with_feature_extraction)
     condda.run_protocol()
--- tests/test_ond.py
+++ tests/test_ond.py
@@ -23,6 +23,22 @@ def ond_config():
         json.dump(dummy_config, open(os.path.join(config_folder, config_name), "w"))
         yield os.path.join(config_folder, config_name)
 
+@pytest.fixture(scope="function")
+def ond_config_with_feature_extraction():
+    """Fixture to create a config file for feature extraction"""
+    with TemporaryDirectory() as feature_dir:
+        with TemporaryDirectory() as config_folder:
+            dummy_config = {
+                "domain": "image_classification",
+                "test_ids": ["OND.1.1.1234"],
+                "novelty_detector_class": "MockDetector",
+                "feature_extraction_only": True,
+                "save_features": True,
+                "feature_save_dir": feature_dir,
+            }
+            config_name = "test_ond_config.json"
+            json.dump(dummy_config, open(os.path.join(config_folder, config_name), "w"))
+            yield os.path.join(config_folder, config_name)
 
 def test_initialize(
     server_setup, get_interface_params, discoverable_plugins, ond_config
@@ -67,4 +83,27 @@ def test_run_protocol(
     ond.run_protocol()
     local_interface = LocalInterface(config_name, config_directory)
     SailOn(discoverable_plugins, "", local_interface, ond_config)
+    ond.run_protocol()
+
+def test_feature_extraction(
+    server_setup, get_interface_params, discoverable_plugins, ond_config_with_feature_extraction
+):
+    """
+    Test feature extraction.
+
+    Args:
+        server_setup (tuple): Tuple containing url and result directory
+        get_interface_params (tuple): Tuple to configure par interface
+        discoverable_plugins (dict): Dictionary with the plugins
+        ond_config_with_feature_extraction (str): Path to json file
+
+    Return:
+        None
+    """
+    config_directory, config_name = get_interface_params
+    par_interface = ParInterface(config_name, config_directory)
+    ond = SailOn(discoverable_plugins, "", par_interface, ond_config_with_feature_extraction)
+    ond.run_protocol()
+    local_interface = LocalInterface(config_name, config_directory)
+    SailOn(discoverable_plugins, "", local_interface, ond_config_with_feature_extraction)
     ond.run_protocol()
