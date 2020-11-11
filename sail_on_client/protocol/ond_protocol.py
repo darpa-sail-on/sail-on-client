a9c87b9cf5e4dd4cafe31ad2bb119f8e69999f1c
ameya.shringi <ameya.shringi@kitware.com>
2020-11-10 20:56:47 -0500

    Add additional util function to remove all files in results

--- sail_on_client/protocol/ond_protocol.py
+++ sail_on_client/protocol/ond_protocol.py
@@ -4,7 +4,7 @@ from tinker.baseprotocol import BaseProtocol
 
 from sail_on_client.protocol.ond_config import OndConfig
 from sail_on_client.errors import RoundError
-from sail_on_client.utils import safe_remove
+from sail_on_client.utils import safe_remove, safe_remove_results
 from sail_on_client.protocol.parinterface import ParInterface
 from itertools import count
 import os
@@ -95,7 +95,6 @@ class SailOn(BaseProtocol):
                 except RoundError:
                     # no more rounds available, this test is done.
                     break
-
                 (
                     self.toolset["features_dict"],
                     self.toolset["logit_dict"],
@@ -128,8 +127,7 @@ class SailOn(BaseProtocol):
 
                 # cleanup the round files
                 safe_remove(self.toolset["dataset"])
-                safe_remove(results["detection"])
-                safe_remove(results["classification"])
+                safe_remove_results(results)
 
             if self.config["save_features"]:
                 feature_dir = self.config["feature_save_dir"]
--- sail_on_client/utils.py
+++ sail_on_client/utils.py
@@ -15,3 +15,17 @@ def safe_remove(file_path: str) -> None:
     """
     if os.path.exists(file_path):
         os.remove(file_path)
+
+
+def safe_remove_results(results: dict) -> None:
+    """
+    Remove results present in a dict
+
+    Args:
+        file_path (str): File path that should be removed
+
+    Return:
+        None
+    """
+    for result_files in results.values():
+        safe_remove(result_files)
