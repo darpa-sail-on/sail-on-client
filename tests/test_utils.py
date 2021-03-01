de5383dcc02f9b646034836ccf71b3e76dcf8000
ameya.shringi <ameya.shringi@kitware.com>
2021-03-01 11:11:04 -0500

    Fix formating issues

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,4 +1,5 @@
-import pytest
+"""Tests for Utilities."""
+
 from tempfile import TemporaryDirectory
 from sail_on_client.protocol.localinterface import LocalInterface
 from sail_on_client.utils import update_harness_parameters, create_baseline
@@ -19,8 +20,9 @@ def test_update_harness_paramerters(get_interface_params):
         config_directory, config_name = get_interface_params
         local_interface = LocalInterface(config_name, config_directory)
         assert local_interface.data_dir != updated_data_dir
-        local_interface = update_harness_parameters(local_interface,
-                                                    {"data_dir": updated_data_dir})
+        local_interface = update_harness_parameters(
+            local_interface, {"data_dir": updated_data_dir}
+        )
         assert local_interface.data_dir == updated_data_dir
 
 
