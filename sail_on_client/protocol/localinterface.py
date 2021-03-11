6ff019d1263506dd8de05848bfdc591a53d3066c
ameya.shringi <ameya.shringi@kitware.com>
2021-03-11 16:00:11 -0500

    Log accuracy for baseline

--- sail_on_client/protocol/localinterface.py
+++ sail_on_client/protocol/localinterface.py
@@ -332,6 +332,7 @@ class LocalInterface(Harness):
                     100,
                     5,
                 )
+                log.info(f"Baseline performance for {test_id}: {ub.repr2(m_acc_baseline)}")
                 m_nrp = arm_ar.m_nrp(m_acc, m_acc_baseline)
                 results["m_nrp"] = m_nrp
         # ######## Document Transcript Evaluation  ###########
