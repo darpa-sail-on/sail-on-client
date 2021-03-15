ce3635b2de7c23861568c0f2ce5ea6ae3ef269cf
ameya.shringi <ameya.shringi@kitware.com>
2021-03-15 13:44:53 -0400

    Add detector_config in the tests

--- tests/test_condda.py
+++ tests/test_condda.py
@@ -18,6 +18,7 @@ def condda_config():
             "domain": "image_classification",
             "test_ids": ["CONDDA.1.1.1234"],
             "novelty_detector_class": "MockDetector",
+            "detector_config": {}
         }
         config_name = "test_condda_config.json"
         json.dump(dummy_config, open(os.path.join(config_folder, config_name), "w"))
@@ -33,6 +34,7 @@ def condda_config_with_feature_extraction():
                 "domain": "image_classification",
                 "test_ids": ["CONDDA.1.1.1234"],
                 "novelty_detector_class": "MockDetector",
+                "detector_config": {},
                 "feature_extraction_only": True,
                 "save_features": True,
                 "save_dir": feature_dir,
--- tests/test_ond.py
+++ tests/test_ond.py
@@ -18,6 +18,7 @@ def ond_config():
             "domain": "image_classification",
             "test_ids": ["OND.1.1.1234"],
             "novelty_detector_class": "MockDetector",
+            "detector_config": {}
         }
         config_name = "test_ond_config.json"
         json.dump(dummy_config, open(os.path.join(config_folder, config_name), "w"))
@@ -33,6 +34,7 @@ def ond_config_with_feature_extraction():
                 "domain": "image_classification",
                 "test_ids": ["OND.1.1.1234"],
                 "novelty_detector_class": "MockDetector",
+                "detector_config": {},
                 "feature_extraction_only": True,
                 "save_features": True,
                 "save_dir": feature_dir,
