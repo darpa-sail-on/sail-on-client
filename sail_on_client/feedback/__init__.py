c9f57d3e3a7156f8aa5bb152ee108cffe709bc33
ameya.shringi <ameya.shringi@kitware.com>
2021-04-08 12:59:35 -0400

    Add a factory function for creating feedback object

--- sail_on_client/feedback/__init__.py
+++ sail_on_client/feedback/__init__.py
@@ -1,1 +1,26 @@
 """Sail On client feedback package."""
+
+from sail_on_client.feedback.image_classification_feedback import ImageClassificationFeedback
+from sail_on_client.feedback.document_transcription_feedback import DocumentTranscriptionFeedback
+from sail_on_client.feedback.activity_recognition_feedback import ActivityRecognitionFeedback
+from typing import Dict, Union
+
+
+def create_feedback_instance(
+    domain: str, feedback_config: Dict
+) -> Union[ImageClassificationFeedback, DocumentTranscriptionFeedback, ActivityRecognitionFeedback]:
+    """
+    Create an instance of feedback object.
+
+    Args:
+        domain: Name of the domain
+        feedback_config: arguments used to instantiate the metric object
+    """
+    if domain == "image_classification":
+        return ImageClassificationFeedback(**feedback_config)
+    elif domain == "activity_recognition":
+        return ActivityRecognitionFeedback(**feedback_config)
+    elif domain == "transcripts":
+        return DocumentTranscriptionFeedback(**feedback_config)
+    else:
+        raise ValueError(f'Domain: "{domain}" is not a real domain.')
