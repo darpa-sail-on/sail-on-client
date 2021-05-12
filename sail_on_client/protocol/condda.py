70c43023713931d026749131d622ba2e3e656d65
ameya.shringi <ameya.shringi@kitware.com>
2021-05-12 10:48:25 -0400

    Fix format error

--- sail_on_client/protocol/condda.py
+++ sail_on_client/protocol/condda.py
@@ -3,7 +3,11 @@
 from sailon_tinker_launcher.deprecated_tinker.baseprotocol import BaseProtocol
 from sail_on_client.protocol.condda_config import ConddaConfig
 from sail_on_client.errors import RoundError
-from sail_on_client.utils import safe_remove, safe_remove_results, update_harness_parameters
+from sail_on_client.utils import (
+    safe_remove,
+    safe_remove_results,
+    update_harness_parameters,
+)
 from sail_on_client.protocol.parinterface import ParInterface
 from sail_on_client.protocol.localinterface import LocalInterface
 from itertools import count
