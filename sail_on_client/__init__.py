1341b582f1e081d5dc8c2d981cc5cd9d583979ea
ameya.shringi <ameya.shringi@kitware.com>
2021-06-22 15:36:21 -0400

    Add __version__ using dunamai

--- sail_on_client/__init__.py
+++ sail_on_client/__init__.py
@@ -1,1 +1,5 @@
 """Sail-on client package."""
+
+from sail_on_client._version import _dynamic_version
+
+__version__ = _dynamic_version
---
+++ sail_on_client/_version.py
@@ -0,0 +1,12 @@
+"""Dynamic version for sail on client."""
+
+from dunamai import Version, Style
+
+version_pattern = r"""
+    (?x)                                                (?# ignore whitespace)
+    ^(?P<base>\d+\.\d+\.\d+)                            (?# 1.2.3)
+    (-?((?P<stage>[a-zA-Z]+)\.?(?P<revision>\d+)?))?    (?# b0)
+    (\+(?P<tagged_metadata>.+))?$                       (?# +linux)
+""".strip()
+
+_dynamic_version = Version.from_any_vcs(pattern=version_pattern).serialize(style=Style.Pep440)
