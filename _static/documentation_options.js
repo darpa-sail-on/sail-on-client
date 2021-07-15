42ba186653280ce53eb08d5533e3c7ba04eb2d1b
as6520 <as6520@users.noreply.github.com>
2021-07-15 21:34:28 +0000

    deploy: 7d189e265457c9e93c49bffb264f011096cb71ac

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.0+1.fa2be88',
+    VERSION: '0.0.0+1.7d189e2',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
