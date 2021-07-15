d3488e775f752b9a8dd5ceb32baa588ad659a72a
ameya.shringi <ameya.shringi@kitware.com>
2021-07-15 11:16:11 -0400

    Fix util tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,23 +1,23 @@
 """Tests for Utilities."""
 
 from tempfile import TemporaryDirectory
-from sail_on_client.protocol.localinterface import LocalInterface
+from sail_on_client.harness.local_harness import LocalHarness
 from sail_on_client.utils.utils import update_harness_parameters
 
 
-def test_update_harness_paramerters(get_interface_params):
+def test_update_harness_paramerters(get_local_harness_params):
     """
     Test to update harness parameters.
 
     Args:
-        get_interface_params (tuple): Tuple to configure local interface
+        get_local_harness_params (tuple): Tuple to configure local interface
 
     Return:
         None
     """
     with TemporaryDirectory() as updated_data_dir:
-        config_directory, config_name = get_interface_params
-        local_interface = LocalInterface(config_name, config_directory)
+        data_dir, gt_dir, gt_config = get_local_harness_params
+        local_interface = LocalHarness(data_dir, gt_dir, gt_config)
         assert local_interface.data_dir != updated_data_dir
         local_interface = update_harness_parameters(
             local_interface, {"data_dir": updated_data_dir}
