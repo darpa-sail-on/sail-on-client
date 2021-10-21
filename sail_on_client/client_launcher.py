c06dc896d1e615cc702ad91ef33df50c2075a86c
ameya.shringi <ameya.shringi@kitware.com>
2021-10-21 10:36:29 -0400

    Update the launcher

--- sail_on_client/client_launcher.py
+++ sail_on_client/client_launcher.py
@@ -1,9 +1,13 @@
 import hydra
+import logging
+import os
 from omegaconf import DictConfig, OmegaConf
-from tinker.configuration import parse_configuration
+from omegaconf.errors import MissingMandatoryValue
+from tinker.configuration import process_config
 
+log = logging.getLogger(__name__)
 
-@hydra.main(config_path="configs")
+@hydra.main(config_path="configs", config_name="default")
 def client_launcher(cfg: DictConfig) -> None:
     """
     Primary entrypoint for the client.
@@ -14,7 +18,17 @@ def client_launcher(cfg: DictConfig) -> None:
     Returns:
         None
     """
-    print(parse_configuration(OmegaConf.to_yaml(cfg)))
+    log.info(cfg)
+    # Hack to set save directory based on working directory
+    try:
+        cfg_dict = OmegaConf.to_container(cfg, resolve=True, throw_on_missing=True)
+    except MissingMandatoryValue as e:
+        # Set save directory to output directory that hydra is using
+        cfg.protocol.smqtk.config.save_dir = os.getcwd()
+    finally:
+        cfg_dict = OmegaConf.to_container(cfg, resolve=True, throw_on_missing=True)
+    protocol = process_config(cfg_dict["protocol"])
+    protocol.run_protocol({})
 
 
 if __name__ == "__main__":
