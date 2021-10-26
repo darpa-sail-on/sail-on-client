071c05d4a86129bb44e2ebfb758f7a5c762044bb
ameya.shringi <ameya.shringi@kitware.com>
2021-10-26 12:53:14 -0400

    Replace == with isclose

--- tests/test_activity_recognition_feedback.py
+++ tests/test_activity_recognition_feedback.py
@@ -2,6 +2,7 @@
 
 import pytest
 import os
+import numpy as np
 
 from sail_on_client.feedback.activity_recognition_feedback import (
     ActivityRecognitionFeedback,
@@ -152,7 +153,7 @@ def test_get_score_feedback(
         10, 10, 10, par_interface, session_id, test_id, protocol_constant
     )
     df_score = feedback.get_feedback(0, list(range(10)), feedback_image_ids)
-    assert df_score[1][0] == 0.1987951807228916
+    assert np.isclose(df_score[1][0], 0.1987951807228916, atol=1e-05)
 
 
 @pytest.mark.parametrize(
