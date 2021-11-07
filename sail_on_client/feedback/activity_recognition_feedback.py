481653d7b2624b8aa9e0780255f9c2d04f169902
ameya.shringi <ameya.shringi@kitware.com>
2021-11-07 15:01:03 -0500

    Update classification to support 5 known classes

--- sail_on_client/feedback/activity_recognition_feedback.py
+++ sail_on_client/feedback/activity_recognition_feedback.py
@@ -4,6 +4,7 @@ from sail_on_client.harness.local_harness import LocalHarness
 from sail_on_client.harness.par_harness import ParHarness
 from sail_on_client.feedback.feedback import Feedback
 from typing import Union
+import pandas as pd
 
 SUPPORTED_FEEDBACK = ["classification", "score"]
 
@@ -47,3 +48,41 @@ class ActivityRecognitionFeedback(Feedback):
             test_id,
             feedback_type,
         )
+
+    def get_labeled_feedback(
+        self, round_id: int, images_id_list: list, image_names: list
+    ) -> Union[pd.DataFrame, None]:
+        """
+        Get labeled feedback for the round.
+
+        Args:
+            round_id: Round identifier
+            image_id_list: List if indices for images
+            image_names: List of image names for the round
+
+        Return:
+            A dictionary with the accuracy value or None if
+            feedback is requested for an older round
+        """
+        if round_id > self.current_round:
+            self.deposit_income()
+            self.current_round = round_id
+            if len(images_id_list) <= self.budget:
+                self.budget = self.budget - len(images_id_list)
+                image_ids = [image_names[int(idx)] for idx in images_id_list]
+                feedback_file = self.interface.get_feedback_request(
+                    image_ids,
+                    self.feedback_type,
+                    self.test_id,
+                    round_id,
+                    self.session_id,
+                )
+                df = pd.read_csv(
+                    feedback_file, delimiter=",", header=None,
+                    names=["id", "class1",  "class2", "class3", "class4", "class5"]
+                )
+            else:
+                raise ValueError("the function should be added")
+        else:
+            df = None
+        return df
