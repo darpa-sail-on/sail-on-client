9f4d08230d2fe996a386eed5c0440b3246ec6138
ameya.shringi <ameya.shringi@kitware.com>
2021-11-26 09:40:29 -0500

    Add detection feedback

--- sail_on_client/feedback/activity_recognition_feedback.py
+++ sail_on_client/feedback/activity_recognition_feedback.py
@@ -3,10 +3,10 @@
 from sail_on_client.harness.local_harness import LocalHarness
 from sail_on_client.harness.par_harness import ParHarness
 from sail_on_client.feedback.feedback import Feedback
-from typing import Union
+from typing import Union, Optional, Dict
 import pandas as pd
 
-SUPPORTED_FEEDBACK = ["classification", "score"]
+SUPPORTED_FEEDBACK = ["classification", "score", "detection"]
 
 
 class ActivityRecognitionFeedback(Feedback):
@@ -88,3 +88,64 @@ class ActivityRecognitionFeedback(Feedback):
         else:
             df = None
         return df
+
+    def get_detection_feedback(
+        self, round_id: int, images_id_list: list, image_names: list
+    ) -> Optional[pd.DataFrame]:
+        """
+        Get detection feedback for the round.
+
+        Args:
+            round_id: Round identifier
+            image_id_list: List of indices for images
+            image_names: List of image names for the round
+
+        Return:
+            A dataframe with id and novelty detection value as columns
+        """
+        if round_id > self.current_round:
+            self.deposit_income()
+            self.current_round = round_id
+            if len(images_id_list) <= self.budget:
+                self.budget = self.budget - len(images_id_list)
+                image_ids = [image_names[int(idx)] for idx in images_id_list]
+                feedback_file = self.interface.get_feedback_request(
+                    image_ids,
+                    self.feedback_type,
+                    self.test_id,
+                    round_id,
+                    self.session_id,
+                )
+                df = pd.read_csv(
+                    feedback_file,
+                    delimiter=",",
+                    header=None,
+                    names=["id", "detection"],
+                )
+            else:
+                raise ValueError("Requesting over budget")
+        else:
+            df = None
+        return df
+
+    def get_feedback(
+        self, round_id: int, images_id_list: list, image_names: list
+    ) -> Union[pd.DataFrame, Dict, None]:
+        """
+        Get feedback for the round.
+
+        Args:
+            round_id: Round identifier
+            image_id_list: List if indices for images
+            image_names: List of image names for the round
+
+        Return:
+            Either a dataframe or dictionary with score if the request is valid
+            for the current round.
+        """
+        if self.feedback_type == "classification" or self.feedback_type == "score":
+            return super().get_feedback(round_id, images_id_list, image_names)
+        elif self.feedback_type == "detection":
+            return self.get_detection_feedback(round_id, images_id_list, image_names)
+        else:
+            raise ValueError("Unsupported feedback type {self.feedback_type} specified")
