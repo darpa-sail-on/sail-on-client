2651612cc508a1c05b4f0956143b495dbf1e18f5
ameya.shringi <ameya.shringi@kitware.com>
2022-01-10 18:20:11 -0500

    Move file provider in harness

---
+++ sail_on_client/harness/constants.py
@@ -0,0 +1,16 @@
+class ProtocolConstants:
+    # Feedback types
+    DETECTION = "detection"
+    CLASSIFICATION = "classification"
+    CHARACTERIZATION = "characterization"
+    TRANSCRIPTION = "transcription"
+    TEMPORAL = "temporal"
+    SPATIAL = "spatial"
+    PSUEDO_CLASSIFICATION = "psuedo_labels_classification"
+    SCORE = "score"
+
+    # Detection requirement
+    REQUIRED = "REQUIRED"
+    NOTIFY_AND_CONTINUE = "NOTIFY_AND_CONTINUE"
+    IGNORE = "IGNORE"
+    SKIP = "SKIP"
